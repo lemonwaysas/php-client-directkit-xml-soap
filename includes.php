880ab6b94e26f2ecd181490e9c8c9f23c71ae691
duongphuhiep <duongphuhiep@gmail.com>
2017-01-02 16:56:42 +0100

    enhance get IP fallback to localhost and add check service online

--- includes.php
+++ includes.php
@@ -5,24 +5,60 @@ define('PASSWORD', '123456');
 define('VERSION', '1.8');
 define('LANGUAGE', 'en');
 define('UA', isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'ua');
-
+define('DIRECTKIT_WSDL', DIRECTKIT_WS."?wsdl");
 /**
  * Get real IP
  * @return real IP
  */
 function getUserIP() {
-    $client = @$_SERVER['HTTP_CLIENT_IP'];
-    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-    $remote = $_SERVER['REMOTE_ADDR'];
-
-    if (filter_var($client, FILTER_VALIDATE_IP)) {
-        $ip = $client;
-    } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
-        $ip = $forward;
+    $ip = '';
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+        $ip = $_SERVER['REMOTE_ADDR'];
     } else {
-        $ip = $remote;
+        $ip = "127.0.0.1";
     }
-
     return $ip;
 }
-?>
+
+/**
+* check if service is online. This method is useless.
+* you should simple try catch the "SoapFault" exception when calling "new SoapClient()" and other services
+*/
+function checkServiceOnline() {
+    $options = array(
+        CURLOPT_URL            => DIRECTKIT_WSDL,
+        CURLOPT_CONNECTTIMEOUT => 60,
+        CURLOPT_TIMEOUT        => 60,
+        CURLOPT_MAXREDIRS      => 10,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        // CURLOPT_HEADER         => true,
+        // CURLOPT_FOLLOWLOCATION => true,
+        // CURLOPT_AUTOREFERER    => true,
+    );
+    
+    $ch = curl_init();
+    curl_setopt_array( $ch, $options );
+    $response = curl_exec($ch); 
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if ( $httpCode == 200 ){
+        return true;
+    } else {
+        $errmsg = "Service unavailable: ".$httpCode." - ".curl_error($ch);
+        throw new Exception($errmsg);
+    }
+}
+
+// if your SoapClient return a fatal error which you couldn't catch it, you can handle the Soap error specifically like this:
+// set_error_handler('handlePhpErrors');
+// function handlePhpErrors($errno, $errmsg, $filename, $linenum, $vars) {
+//     if (stristr($errmsg, "SoapClient::SoapClient")) {
+//          error_log($errmsg); // silently log error
+//          return; // skip error handling
+//     }
+// }
